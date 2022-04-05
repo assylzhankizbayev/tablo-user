227db990c65a79bd35daf04227d01c64cae8861a
Assylzhan Kizbayev <AKizbayev@one.kz>
2022-04-05 17:26:37 +0600

    fix: fixed unsubscription and data init

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,36 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { WebSocketService } from '../../services/web-socket.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
   totalUserAmount = 0;
   currentUserId = 0;
+  destroy$ = new Subject();
 
-  constructor(private webSocket: WebSocketService) { }
+  constructor(private webSocket: WebSocketService) {}
 
   ngOnInit(): void {
-    this.webSocket.listen('connect').subscribe(() => console.log('Connection succeeded'));
+    this.webSocket
+      .listen('connect')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => console.log('Connection succeeded'));
 
-    this.webSocket.listen('init').subscribe(({ regIdx, calIdx }) => {
-      console.log('init', regIdx, calIdx);
+    this.webSocket
+      .listen('init')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(({ regIdx, calIdx }) => {
+        console.log('init', regIdx, calIdx);
 
-      this.totalUserAmount = regIdx || 0;
-      this.currentUserId = calIdx || 0;
-    });
+        this.totalUserAmount = regIdx || 0;
+        this.currentUserId = calIdx || 0;
+      });
 
-    this.webSocket.listen('user-info').subscribe(data => {
-      this.currentUserId = data.id;
-      console.log('Current user id', this.currentUserId);
-    });
+    this.webSocket
+      .listen('user-info')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data) => {
+        this.currentUserId = data.id;
+        console.log('Current user id', this.currentUserId);
+      });
 
-    this.webSocket.listen('registered-users-amount').subscribe(data => {
-      this.totalUserAmount = data.amount;
-      console.log('Accept total users amount', this.totalUserAmount);
-    });
+    this.webSocket
+      .listen('registered-users-amount')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data) => {
+        this.totalUserAmount = data.amount;
+        console.log('Accept total users amount', this.totalUserAmount);
+      });
   }
 
   next(): void {
@@ -38,5 +53,10 @@ export class MainComponent implements OnInit {
       this.currentUserId++;
       this.webSocket.emit('call-next-user', null);
     }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 }
